9340000ea67c06540e5e4df08d6adb6436d66a74
leandro <leandro@pop-os.localdomain>
2023-09-18 11:13:20 -0300

    create students list

--- src/views/StudentsList.vue
+++ src/views/StudentsList.vue
@@ -1,5 +1,43 @@
 <template>
-
+  <v-container>
+    <v-table>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Nome
+          </th>
+          <th class="text-left">
+            Email
+          </th>
+          <th class="text-left">
+            Matrícula
+          </th>
+          <th class="text-left">
+            Nota
+          </th>
+          <th class="text-left">
+            Ações
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="student in students"
+          :key="student.name"
+        >
+          <td>{{ student.name }}</td>
+          <td>{{ student.email }}</td>
+          <td>{{ student.registration }}</td>
+          <td>{{ student.grade }}</td>
+          <td>
+            <v-col cols="auto">
+              <v-btn density="comfortable" icon="mdi-pencil-outline"></v-btn>
+            </v-col>
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
+  </v-container>
 </template>
 
 <script>
@@ -8,16 +46,21 @@ import api from '@/utils/api.js'
 export default {
   data() {
     return {
-      user: null,
+      students: [],
       loading: {
         login: false,
       }
     };
   },
-  create() {
-    console.log('user');
-    this.user = localStorage.getItem('user');
-    console.log(this.user);
+  created() {
+    api.get('/student/list')
+      .then((response) => {
+        this.students = response.data.data;
+      })
+      .catch((e) => {
+        console.log('error');
+        console.log(e);
+      });
   },
   methods: {
 
