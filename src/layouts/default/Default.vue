b1b3db9e6aa7d2a628556f563dab638be58225be
leandro <leandro@pop-os.localdomain>
2023-09-18 14:04:52 -0300

    changes on main page

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,49 +1,21 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      permanent
-      floating
-      flat
-    >
-      <template v-slot:prepend>
-        <v-list-item
-          v-if="user"
-          lines="two"
-          prepend-avatar="https://randomuser.me/api/portraits/lego/1.jpg"
-          :title="user.name"
-          subtitle="Bem vindo!"
-        ></v-list-item>
-      </template>
-
-      <v-list color="transparent">
-        <v-list-item prepend-icon="mdi-view-dashboard" title="Lista de alunos"></v-list-item>
-      </v-list>
-
-      <template v-slot:append>
-        <div class="pa-2">
-          <v-btn
-            color="blue"
-            block
-            @click="logout"
-            :loading="loading.logout"
-          >
-            Logout
-          </v-btn>
-        </div>
-      </template>
-
-    </v-navigation-drawer>
-
     <v-app-bar flat>
-      <v-app-bar-nav-icon @click="drawer = !drawer"/>
-
       <v-app-bar-title>Students Manager</v-app-bar-title>
 
       <template v-slot:append>
         <v-btn icon="mdi-github"></v-btn>
 
-        <v-btn icon="mdi-dots-vertical"></v-btn>
+
+        <v-btn
+          prepend-icon="mdi-logout"
+          @click="logout"
+        >
+          <template v-slot:prepend>
+            <v-icon color="black"></v-icon>
+          </template>
+          Logout
+        </v-btn>
       </template>
     </v-app-bar>
 
@@ -57,7 +29,6 @@ import api from '@/utils/api.js'
 
 export default {
   data: () => ({
-    drawer: null,
     loading: {
       logout: false,
     },
@@ -68,7 +39,7 @@ export default {
   },
   methods: {
     logout() {
-      
+
       this.loading.logout = true;
 
       api.post('/auth/logout')
